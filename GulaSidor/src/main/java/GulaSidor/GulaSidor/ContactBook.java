460c975bbb21710f4250a07dfc40ce98330a4146
Simon Södergren <simon.sodergren@live.se>
2020-02-05 14:25:06 +0100

    AddRemoveSearch uppdaterad och ContactBook

--- GulaSidor/src/main/java/GulaSidor/GulaSidor/ContactBook.java
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/ContactBook.java
@@ -4,8 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactBook {
+    
 
-    ArrayList<Person> p = new ArrayList<Person>(); 
+    ArrayList<Person> p = new ArrayList<Person>();
+
+    public ContactBook() {
+        LoadSave ls = new LoadSave(); 
+        
+        p = ls.Load();
+    }
 
     
 
