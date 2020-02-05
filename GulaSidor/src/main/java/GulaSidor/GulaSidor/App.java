4c5322e8300d4d9bdf9c29a88923e5e606648a24
OnlySchnitzel <peter.ihm98@gmail.com>
2020-02-05 12:58:08 +0100

    First Commit Setup Project

---
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/App.java
@@ -0,0 +1,13 @@
+package GulaSidor.GulaSidor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
