7f9d4c817192b62613dcdf46f09e8b0eb6f39efc
OnlySchnitzel <peter.ihm98@gmail.com>
2020-02-05 13:55:36 +0100

    Lagt till Basic code för LoadSave och Person

---
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/LoadSave.java
@@ -0,0 +1,36 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import com.google.gson.Gson;
+
+public class LoadSave{
+
+	private String path = "Contacts.json";
+
+    public ContactBook Load()
+	{
+		try {
+			Gson gson = new Gson();
+			return gson.fromJson(new FileReader(path), ContactBook.class);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return new ContactBook();
+	}
+
+	public void Save(ContactBook contacts)
+	{
+		Gson gson = new Gson();
+		String json = gson.toJson(contacts);
+
+		try {
+			FileWriter writer = new FileWriter(path);
+			writer.write(json);
+			writer.close();
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
