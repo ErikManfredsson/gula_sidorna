7c1117a6ffe10463f459d8a8a6712b45c85d5af9
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-02-06 15:01:09 +0100

    Changed colorscheme, added a .jpg logo on top

--- GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
@@ -1,15 +1,16 @@
 package GulaSidor.GulaSidor;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,14 +27,15 @@ public class GUI {
 	private JPanel controlPanel;
 	private DefaultListModel defaultListModel;
 	private JTextField textField;
-	
+	private ImageIcon image;
+
 	public GUI() {
 		GUI();
 	}
 
 	public void GUI() {
 		mainFrame = new JFrame("Contact Book");
-		mainFrame.setSize(400, 600);
+		mainFrame.setSize(600, 900);
 		mainFrame.setLayout(new FlowLayout());
 		textField = new JTextField();
 		
@@ -75,57 +77,80 @@ public class GUI {
 		defaultListModel.addElement("Peter 031-1337");
 		defaultListModel.addElement("Simon 031-112");
 		
+		image = new ImageIcon("C:\\Users\\Ereko\\eclipse-workspace\\gula_sidorna\\GulaSidor\\eniro.jpg");
+		JLabel label = new JLabel("", image, JLabel.CENTER);
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(label, BorderLayout.CENTER);
+		mainFrame.add(panel);
 		
 		JList<String> list = new JList<String>(defaultListModel);
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setVisibleRowCount(-1);
 		JScrollPane listScroller = new JScrollPane(list);
-		listScroller.setPreferredSize(new Dimension(250, 300));
+		listScroller.setPreferredSize(new Dimension(450, 400));
 		list.setBounds(100, 100, 75, 75);
 		mainFrame.add(listScroller);
-		textField.setPreferredSize(new Dimension(250, 20));
-		textField.setBackground(new Color(250, 243, 25));
-		list.setBackground(new Color(250, 243, 25));
+		textField.setPreferredSize(new Dimension(450, 30));
+		textField.setBackground(new Color(255, 224, 1));
+		list.setBackground(new Color(255, 224, 1));
 		mainFrame.add(textField);
+		
 		headerLabel = new JLabel("", JLabel.CENTER);
 		statusLabel = new JLabel("", JLabel.CENTER);
 		statusLabel.setSize(300, 300);
 
+//	ImageIcon image = new ImageIcon("image/pic1.jpg");
+//	JLabel label = new JLabel("", image, JLabel.CENTER);
+//	JPanel panel = new JPanel(new BorderLayout());
+//	panel.add( label, BorderLayout.CENTER );
+		
 		mainFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
 				System.exit(0);
 			}
 		});
 
-		controlPanel = new JPanel();
+		controlPanel = new JPanel(new BorderLayout());
 		controlPanel.setLayout(new FlowLayout());
 		
 		mainFrame.add(headerLabel);
 		mainFrame.add(controlPanel);
 		mainFrame.add(statusLabel);
 		mainFrame.setVisible(true);
-		mainFrame.setBackground(new Color(250, 243, 25));
-//		mainFrame.setContentPane(controlPanel);
-		controlPanel.setBackground(new Color(250, 243, 25));
-	}
+
+
+		controlPanel.setBackground(new Color(255, 224, 1));
+		headerLabel.setBackground(new Color(255, 224, 1));
+		mainFrame.setBackground(new Color(255, 224, 1));
+		statusLabel.setBackground(new Color(255, 224, 1));
+		mainFrame.getContentPane().setBackground(new Color(255, 224, 1));
+		
+		}
+	
+	
 
 	private void eventDemo() {
 //		headerLabel.setText("headerLabel setText");
-		
+
 		JButton chooseButton = new JButton("Choose");
 		JButton addButton = new JButton("Add");
 		JButton searchButton = new JButton("Search");
 		JButton removeButton = new JButton("Remove");
-
+		
+		chooseButton.setPreferredSize(new Dimension(100,40));
+		addButton.setPreferredSize(new Dimension(100,40));
+		searchButton.setPreferredSize(new Dimension(100,40));
+		removeButton.setPreferredSize(new Dimension(100,40));
+		
 		chooseButton.setActionCommand("Choose");
 		addButton.setActionCommand("Add");
 		searchButton.setActionCommand("Search");
 		removeButton.setActionCommand("Remove");
-		
-		chooseButton.setBackground(new Color(250, 243, 25));
-		addButton.setBackground(new Color(250, 243, 25));
-		searchButton.setBackground(new Color(250, 243, 25));
-		removeButton.setBackground(new Color(250, 243, 25));
+
+		chooseButton.setBackground(new Color(255, 224, 1));
+		addButton.setBackground(new Color(255, 224, 1));
+		searchButton.setBackground(new Color(255, 224, 1));
+		removeButton.setBackground(new Color(255, 224, 1));
 
 		chooseButton.addActionListener(new ButtonClickListener());
 		addButton.addActionListener(new ButtonClickListener());
@@ -144,7 +169,7 @@ public class GUI {
 	private class ButtonClickListener implements ActionListener {
 
 		AddRemoveSearch ars = new AddRemoveSearch();
-		
+
 		public void actionPerformed(ActionEvent e) {
 
 			String command = e.getActionCommand();
@@ -161,6 +186,7 @@ public class GUI {
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 		GUI gui = new GUI();
 		gui.eventDemo();
