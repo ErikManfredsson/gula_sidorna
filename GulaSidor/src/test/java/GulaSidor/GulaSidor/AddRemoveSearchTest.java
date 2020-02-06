f36e57cc5e84e88b12014bc743274dad110a6175
Simon Södergren <simon.sodergren@live.se>
2020-02-06 15:02:47 +0100

    Försökt skriva JUnit test på ARS-klassen

--- GulaSidor/src/test/java/GulaSidor/GulaSidor/AddRemoveSearchTest.java
+++ GulaSidor/src/test/java/GulaSidor/GulaSidor/AddRemoveSearchTest.java
@@ -1,21 +1,39 @@
 package GulaSidor.GulaSidor;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class AddRemoveSearchTest {
 
+	
 	private Person person; 
 	private ContactBook contactBook;
-
-	@BeforeEach 
-	public void addPerson() {
-	Person person = new Person(); 
-	person.name = "Simon Södergren"; 
-	person.number = "0721641288"; 
-	contactBook = new ContactBook(); 
+	private AddRemoveSearch ars; 
+	
+	
+	@Test 
+	public void testFindPerson() throws IndexOutOfBoundsException {
+		Person p2 = new Person(); 
+		p2.name="Simon Södergren"; 
+		p2.number="0721641288"; 
+		AddRemoveSearch ars1 = new AddRemoveSearch();
+		ars1.foundpeople.add(p2);
+		ars1.Search("Simon Södergren", "0721641288");
+		System.out.println(ars1.foundpeople.get(0));
+		assertEquals(ars1.foundpeople.get(0), p2);
 	}
-
-
+	
+//	@Test
+//	public void testFindPersonThatDoesntExist() throws IndexOutOfBoundsException {
+//	Person p3 = new Person(); 
+//	p3.name="Simon Södergren"; 
+//	p3.number="0721641288"; 
+//	AddRemoveSearch ars2 = new AddRemoveSearch(); 
+//	ars2.foundpeople.add(p3); 
+//	ars2.Search("Johannes Göranzon", "0725858585");
+//	assertNotSame(ars2.foundpeople.get(0), p3); 
+//	}
 	
 }
+
