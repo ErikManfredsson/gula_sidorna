6bd896dd281f3353e44e2eb906e127dd931679ba
Simon Södergren <simon.sodergren@live.se>
2020-02-06 14:00:46 +0100

    JUnit testerna klar på ena och startat på andra klassen

--- GulaSidor/src/test/java/GulaSidor/GulaSidor/AddRemoveSearchTest.java
+++ GulaSidor/src/test/java/GulaSidor/GulaSidor/AddRemoveSearchTest.java
@@ -1,8 +1,21 @@
 package GulaSidor.GulaSidor;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class AddRemoveSearchTest extends TestCase {
+public class AddRemoveSearchTest {
+
+	private Person person; 
+	private ContactBook contactBook;
+
+	@BeforeEach 
+	public void addPerson() {
+	Person person = new Person(); 
+	person.name = "Simon Södergren"; 
+	person.number = "0721641288"; 
+	contactBook = new ContactBook(); 
+	}
+
 
 	
 }
