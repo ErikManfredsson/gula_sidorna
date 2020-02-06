6a485af6e9c485f4b90c365b7c049f8634431021
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-02-06 13:59:26 +0100

    Changing colors in GUI-frame

--- GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
@@ -1,5 +1,6 @@
 package GulaSidor.GulaSidor;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -15,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class GUI {
 
@@ -23,6 +25,7 @@ public class GUI {
 	private JLabel statusLabel;
 	private JPanel controlPanel;
 	private DefaultListModel defaultListModel;
+	private JTextField textField;
 	
 	public GUI() {
 		GUI();
@@ -31,10 +34,11 @@ public class GUI {
 	public void GUI() {
 		mainFrame = new JFrame("Contact Book");
 		mainFrame.setSize(400, 600);
-		mainFrame.setLayout(new GridLayout(4, 1));
+		mainFrame.setLayout(new FlowLayout());
+		textField = new JTextField();
 		
 		DefaultListModel <String> defaultListModel = new DefaultListModel<String>();
-		defaultListModel.addElement("Erik 0768765118")
+		defaultListModel.addElement("Erik 0768765118");
 		defaultListModel.addElement("Peter 031-1337");
 		defaultListModel.addElement("Simon 031-112");
 		defaultListModel.addElement("Erik 0768765118");
@@ -76,14 +80,16 @@ public class GUI {
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setVisibleRowCount(-1);
 		JScrollPane listScroller = new JScrollPane(list);
-		listScroller.setPreferredSize(new Dimension(250, 80));
+		listScroller.setPreferredSize(new Dimension(250, 300));
 		list.setBounds(100, 100, 75, 75);
 		mainFrame.add(listScroller);
-
-	
-		headerLabel = new JLabel("headerLabel new JLabel", JLabel.CENTER);
-		statusLabel = new JLabel("statusLabel new JLabel", JLabel.CENTER);
-		statusLabel.setSize(300, 100);
+		textField.setPreferredSize(new Dimension(250, 20));
+		textField.setBackground(new Color(250, 243, 25));
+		list.setBackground(new Color(250, 243, 25));
+		mainFrame.add(textField);
+		headerLabel = new JLabel("", JLabel.CENTER);
+		statusLabel = new JLabel("", JLabel.CENTER);
+		statusLabel.setSize(300, 300);
 
 		mainFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
@@ -93,15 +99,18 @@ public class GUI {
 
 		controlPanel = new JPanel();
 		controlPanel.setLayout(new FlowLayout());
-
+		
 		mainFrame.add(headerLabel);
 		mainFrame.add(controlPanel);
 		mainFrame.add(statusLabel);
 		mainFrame.setVisible(true);
+		mainFrame.setBackground(new Color(250, 243, 25));
+//		mainFrame.setContentPane(controlPanel);
+		controlPanel.setBackground(new Color(250, 243, 25));
 	}
 
 	private void eventDemo() {
-		headerLabel.setText("headerLabel setText");
+//		headerLabel.setText("headerLabel setText");
 		
 		JButton chooseButton = new JButton("Choose");
 		JButton addButton = new JButton("Add");
@@ -112,6 +121,11 @@ public class GUI {
 		addButton.setActionCommand("Add");
 		searchButton.setActionCommand("Search");
 		removeButton.setActionCommand("Remove");
+		
+		chooseButton.setBackground(new Color(250, 243, 25));
+		addButton.setBackground(new Color(250, 243, 25));
+		searchButton.setBackground(new Color(250, 243, 25));
+		removeButton.setBackground(new Color(250, 243, 25));
 
 		chooseButton.addActionListener(new ButtonClickListener());
 		addButton.addActionListener(new ButtonClickListener());
@@ -129,7 +143,7 @@ public class GUI {
 
 	private class ButtonClickListener implements ActionListener {
 
-		
+		AddRemoveSearch ars = new AddRemoveSearch();
 		
 		public void actionPerformed(ActionEvent e) {
 
@@ -139,6 +153,7 @@ public class GUI {
 				statusLabel.setText("Du tryckte Choose");
 			} else if (command.equals("Add")) {
 				statusLabel.setText("Du tryckte Add");
+				statusLabel.setText(textField.getText());
 			} else if (command.equals("Search")) {
 				statusLabel.setText("Du tryckte Search");
 			} else if (command.equals("Remove")) {
