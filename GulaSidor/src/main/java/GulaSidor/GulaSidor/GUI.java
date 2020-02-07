eb17acce5a54de704415a2cf9b872acc6d7647a4
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-02-07 13:48:30 +0100

    fixed textfieldname & number, erase text on click

--- GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
+++ GulaSidor/src/main/java/GulaSidor/GulaSidor/GUI.java
@@ -4,8 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -35,7 +38,7 @@ public class GUI {
 	public GUI() {
 
 		LoadSave ls = new LoadSave();
-        cb = ls.Load();
+		cb = ls.Load();
 		GUI();
 	}
 
@@ -43,13 +46,13 @@ public class GUI {
 		mainFrame = new JFrame("Contact Book");
 		mainFrame.setSize(600, 900);
 		mainFrame.setLayout(new FlowLayout());
-		textFieldName= new JTextField("Sök efter namn...");
+		textFieldName = new JTextField("Sök efter namn...");
 		textFieldNumber = new JTextField("Sök efter telefonnummer...");
-		
-		DefaultListModel <String> defaultListModel = new DefaultListModel<String>();
+
+		DefaultListModel<String> defaultListModel = new DefaultListModel<String>();
 
 		for (int j = 0; j < cb.p.size(); j++) {
-			defaultListModel.addElement(cb.p.get(j).fname + " " + cb.p.get(j).lname + " " + cb.p.get(j).number); 
+			defaultListModel.addElement(cb.p.get(j).fname + " " + cb.p.get(j).lname + " " + cb.p.get(j).number);
 		}
 
 		image = new ImageIcon("src\\main\\java\\GulaSidor\\GulaSidor\\eniro.jpg");
@@ -57,21 +60,26 @@ public class GUI {
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.add(label, BorderLayout.CENTER);
 		mainFrame.add(panel);
-		
+
 		JList<String> list = new JList<String>(defaultListModel);
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setVisibleRowCount(-1);
 		JScrollPane listScroller = new JScrollPane(list);
 		listScroller.setPreferredSize(new Dimension(450, 400));
+		list.setFont(new java.awt.Font("Arial", Font.PLAIN, 14));
+
 		list.setBounds(100, 100, 75, 75);
 		mainFrame.add(listScroller);
 		textFieldName.setPreferredSize(new Dimension(225, 30));
 		textFieldNumber.setPreferredSize(new Dimension(225, 30));
+		textFieldName.setFont(new java.awt.Font("Arial", Font.BOLD, 14));
+		textFieldNumber.setFont(new java.awt.Font("Arial", Font.BOLD, 14));
+
 //		textField.setBackground(new Color(255, 224, 1));
 		list.setBackground(new Color(255, 224, 1));
 		mainFrame.add(textFieldName);
 		mainFrame.add(textFieldNumber);
-		
+
 		headerLabel = new JLabel("", JLabel.CENTER);
 		statusLabel = new JLabel("", JLabel.CENTER);
 		statusLabel.setSize(300, 300);
@@ -80,7 +88,7 @@ public class GUI {
 //	JLabel label = new JLabel("", image, JLabel.CENTER);
 //	JPanel panel = new JPanel(new BorderLayout());
 //	panel.add( label, BorderLayout.CENTER );
-		
+
 		mainFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
 				System.exit(0);
@@ -89,36 +97,32 @@ public class GUI {
 
 		controlPanel = new JPanel(new BorderLayout());
 		controlPanel.setLayout(new FlowLayout());
-		
+
 		mainFrame.add(headerLabel);
 		mainFrame.add(controlPanel);
 		mainFrame.add(statusLabel);
 		mainFrame.setVisible(true);
-
 
 		controlPanel.setBackground(new Color(255, 224, 1));
 		headerLabel.setBackground(new Color(255, 224, 1));
 		mainFrame.setBackground(new Color(255, 224, 1));
 		statusLabel.setBackground(new Color(255, 224, 1));
 		mainFrame.getContentPane().setBackground(new Color(255, 224, 1));
-		
-		}
-	
-	
+
+	}
 
 	private void eventDemo() {
-//		headerLabel.setText("headerLabel setText");
 
 		JButton chooseButton = new JButton("Choose");
 		JButton addButton = new JButton("Add");
 		JButton searchButton = new JButton("Search");
 		JButton removeButton = new JButton("Remove");
-		
-		chooseButton.setPreferredSize(new Dimension(100,40));
-		addButton.setPreferredSize(new Dimension(100,40));
-		searchButton.setPreferredSize(new Dimension(100,40));
-		removeButton.setPreferredSize(new Dimension(100,40));
-		
+
+		chooseButton.setPreferredSize(new Dimension(100, 40));
+		addButton.setPreferredSize(new Dimension(100, 40));
+		searchButton.setPreferredSize(new Dimension(100, 40));
+		removeButton.setPreferredSize(new Dimension(100, 40));
+
 		chooseButton.setActionCommand("Choose");
 		addButton.setActionCommand("Add");
 		searchButton.setActionCommand("Search");
@@ -143,47 +147,58 @@ public class GUI {
 
 //	}
 //	final AddRemoveSearch ars = new AddRemoveSearch();
-
-	chooseButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent event) {
-//			ars.Add(textField.getText(), textField.getText());
-			System.out.println("Du tryckte choose");
-		}
-	});
-	addButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent event) {
-			System.out.println("Du tryckte add");
-		}
-	});
-	searchButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent event) {
-			System.out.println("Du tryckte search");
-			ars.Search(textFieldName.getText() , textFieldNumber.getText());
-			
-			System.out.println(ars.foundpeople.size());
-
-			for (int i = 0; i < ars.foundpeople.size() ; i++) {
-				//defaultListModel.addElement(ars.foundpeople.get(i).name + " " + ars.foundpeople.get(i).number); 
-				//System.out.println(ars.foundpeople.get(i).name + " " + ars.foundpeople.get(i).number);
-				System.out.println("hello world");
+		
+		textFieldName.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+			    textFieldName.setText("");
 			}
-		}
-	});
-	removeButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent event) {
-			System.out.println("Du tryckte remove");
-		}
-	});
+		});
+		
+		textFieldNumber.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+			    textFieldNumber.setText("");
+			}
+		});
 
+		chooseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+//			ars.Add(textField.getText(), textField.getText());
+				System.out.println("Du tryckte choose");
+			}
+		});
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.out.println("Du tryckte add");
+			}
+		});
+		searchButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.out.println("Du tryckte search");
+				ars.Search(textFieldName.getText(), textFieldNumber.getText());
+
+				System.out.println(ars.foundpeople.size());
+
+				for (int i = 0; i < ars.foundpeople.size(); i++) {
+					// defaultListModel.addElement(ars.foundpeople.get(i).name + " " +
+					// ars.foundpeople.get(i).number);
+					// System.out.println(ars.foundpeople.get(i).name + " " +
+					// ars.foundpeople.get(i).number);
+					System.out.println("hello world");
+				}
+			}
+		});
+		removeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				System.out.println("Du tryckte remove");
+			}
+		});
 
 	}
 
 	public static void main(String[] args) {
 
-
 		GUI gui = new GUI();
 		gui.eventDemo();
-
 
 	}
 }
