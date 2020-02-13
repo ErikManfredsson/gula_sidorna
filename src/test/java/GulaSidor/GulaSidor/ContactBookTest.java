70f13f981cb56a3fd37ac2947d904e335e7a7653
Simon Södergren <simon.sodergren@live.se>
2020-02-13 13:27:36 +0100

    Github suger delete later

--- src/test/java/GulaSidor/GulaSidor/ContactBookTest.java
+++ src/test/java/GulaSidor/GulaSidor/ContactBookTest.java
@@ -15,7 +15,8 @@ public class ContactBookTest  {
 	@BeforeEach 
 	public void addPerson() {
 	Person person = new Person(); 
-	person.name = "Simon Södergren"; 
+	person.fname = "Simon"; 
+	person.lname="Södergren";
 	person.number = "0721641288"; 
 	contactBook = new ContactBook(); 
 	}
@@ -29,7 +30,8 @@ public class ContactBookTest  {
 	@Test 
 	public void testNotEqualPersonsInContactBook() {
 		Person p2 = new Person(); 
-		p2.name = "Peter Ihm"; 
+		p2.fname = "Peter"; 
+		p2.lname="Ihm";
 		p2.number = "0721585813"; 
 		contactBook.p.add(person); 
 		assertNotEquals(person, p2);
@@ -37,8 +39,9 @@ public class ContactBookTest  {
 	@Test 
 	public void testTwoEqualPersonsInContactBook() {
 		Person p3 = new Person(); 
-		p3.name="Simon Södergren"; 
-		p3.name="0721641288"; 
+		p3.fname="Simon"; 
+		p3.lname="Södergren"; 
+		p3.number="0721641288"; 
 		contactBook.p.add(p3); 
 		assertEquals(p3, contactBook.p.get(0)); 
 	}
@@ -46,7 +49,8 @@ public class ContactBookTest  {
 	@Test 
 	public void RemovePerson() {
 	Person p2 = new Person(); 
-	p2.name = "Erik Manfredsson"; 
+	p2.fname = "Erik"; 
+	p2.lname="Manfredsson";
 	p2.number = "07521285959"; 
 	contactBook = new ContactBook();
 	contactBook.p.remove(p2); 
